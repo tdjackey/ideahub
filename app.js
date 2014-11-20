4545da926526f297bb724bae87dba5ffc44040e3
tdjackey <tdjackey@gmail.com>
2014-11-20 22:03:37 +0800

    use https

--- app.js
+++ app.js
@@ -107,4 +107,14 @@ if (!module.parent) {
 
 
 module.exports = app;
+var https = require('https')
+    ,fs = require("fs");
 
+var options = {
+    key: fs.readFileSync('./privatekey.pem'),
+    cert: fs.readFileSync('./certificate.pem')
+};
+
+https.createServer(options, app).listen(3011, function () {
+    console.log('Https server listening on port ' + 3011);
+});
